c5c4f73a966e2fdb2727e2edccadeed222fdb569
Ishini Gamage <ishinisavishka@novatechzone.com>
2024-02-26 19:39:12 +0530

    final commit

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@ self.addEventListener('install', function(event) {
       caches.open('bookmark-v1').then(function(cache) {
         return cache.addAll([
           '/',
-          '/index.html',
+          '/index.php',
           '/style.css',
           '/script.js',
           '/icon.png'
